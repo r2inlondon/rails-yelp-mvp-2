4780a868b25390c0ecc65bde69ed5416d8d60ee4
arturo quiroz <r2inlondon@gmail.com>
2021-02-15 19:28:42 -0600

    models, validations, seeds - completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating Restaurants with FAKER'
+
+10.times do
+  new_rest = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::Number.number(digits: 8),
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  new_rest.save!
+  puts "Restaurant - #{new_rest.name} save!"
+end
+
+puts 'listoooo!'
