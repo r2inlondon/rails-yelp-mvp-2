a51ccb427a4a5dff43960da608344a353968af99
arturo quiroz <r2inlondon@gmail.com>
2021-02-17 20:34:30 -0600

    review model and controller -done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  before_action :find_restaurant
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root to: 'restaurants#index'
   get 'restaurants', to: 'restaurants#index'
   get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  post 'restaurants', to: 'restaurants#create'
 
+  get  'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
 end
