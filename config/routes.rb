3be02bec1fbac30eec7154a73149788f29376b8f
arturo quiroz <r2inlondon@gmail.com>
2021-02-18 07:51:13 -0600

    review form has been embded into restaurant/show

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
   post 'restaurants', to: 'restaurants#create'
 
-  get  'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+  # get  'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
   post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
 end
