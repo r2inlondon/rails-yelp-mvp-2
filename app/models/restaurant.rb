e3b52f6d1109fcc1dc84df7710edf9975fdecd7f
arturo quiroz <r2inlondon@gmail.com>
2021-02-19 20:11:18 -0600

    styled up with bootstrap and fontawesome

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   has_many :reviews, dependent: :destroy
   validates :name, :address, :phone_number, presence: true
-  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  # validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  validates :category, inclusion: { in: CATEGORIES }
 end
